099d4d901d03f6bde0597129183635ccb25dbdd8
iponir3 <ishtyaqponir@gmail.com>
2019-08-31 13:39:08 -0400

    Update GUI for both apps; *Need to resolve showing progress bar on web app*

--- platforms/android/app/build/generated/source/r/debug/com/exerBase/test/R.java
+++ platforms/android/app/build/generated/source/r/debug/com/exerBase/test/R.java
@@ -16,19 +16,22 @@ public final class R {
     public static final int ic_action_remove=0x7f010004;
     public static final int screen=0x7f010005;
   }
+  public static final class id {
+    public static final int text_id=0x7f020000;
+  }
   public static final class layout {
-    public static final int main_layout=0x7f020000;
+    public static final int main_layout=0x7f030000;
   }
   public static final class mipmap {
-    public static final int icon=0x7f030000;
+    public static final int icon=0x7f040000;
   }
   public static final class string {
-    public static final int activity_name=0x7f040000;
-    public static final int app_name=0x7f040001;
-    public static final int launcher_name=0x7f040002;
+    public static final int activity_name=0x7f050000;
+    public static final int app_name=0x7f050001;
+    public static final int launcher_name=0x7f050002;
   }
   public static final class xml {
-    public static final int config=0x7f050000;
-    public static final int provider_paths=0x7f050001;
+    public static final int config=0x7f060000;
+    public static final int provider_paths=0x7f060001;
   }
 }
